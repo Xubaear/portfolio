ffb7fb4e666393b5cdc6aa56a98c640f5167b0ae
Xubaear <zubaear.hasan7@gmail.com>
2026-01-05 17:06:20 +0600

    about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,9 +11,7 @@ export default function About() {
       >
         <h3 className="text-2xl font-semibold mb-4 mt-20">About me</h3>
         <p className="text-slate-300 max-w-2xl mb-20">
-          I'm a frontend developer who loves building fast, accessible web
-          experiences. I specialise in React, animation, and clean UI design.
-          I enjoy collaborating on products that solve real user problems.
+         Frontend Developer with a passion for React and UI motion. I transform creative designs into interactive, responsive web applications. Beyond the frontend, I am actively learning the MERN stack to build full-scale applications. Open to opportunities where I can contribute, learn, and grow
         </p>
       </motion.div>
     </section>
